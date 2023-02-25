3118bfbfb708431654b14734f64239fc1a8dc26c
Quentin Torroba <torroba.q@gmail.com>
2023-02-25 23:04:57 +0100

    Fix: lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,10 @@
 module.exports = {
   root: true,
   env: {
-    browser: false,
-    es6: true,
     node: true,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
-    tsconfigRootDir: __dirname,
     sourceType: "module",
     ecmaVersion: 2022,
   },
@@ -16,7 +12,6 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "prettier",
   ],
 };
